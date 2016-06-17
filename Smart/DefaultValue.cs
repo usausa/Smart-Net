6385d9f85c10250d487ac4328be963d2f10c23b8
usausa <machi.pon@gmail.com>
2016-06-17 16:52:02 +0900

    Add DefaultValue

---
+++ Smart/DefaultValue.cs
@@ -0,0 +1,47 @@
+﻿namespace Smart
+{
+    using System;
+    using System.Collections.Concurrent;
+#if PCL
+    using System.Reflection;
+#endif
+
+    /// <summary>
+    ///
+    /// </summary>
+    public static class DefaultValue
+    {
+        private static readonly ConcurrentDictionary<Type, object> Cache = new ConcurrentDictionary<Type, object>();
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static object Of(Type type)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+#if PCL
+            if (type.GetTypeInfo().IsValueType && (Nullable.GetUnderlyingType(type) == null))
+#else
+            if (type.IsValueType && (Nullable.GetUnderlyingType(type) == null))
+#endif
+            {
+                object value;
+                if (!Cache.TryGetValue(type, out value))
+                {
+                    value = Activator.CreateInstance(type);
+                    Cache[type] = value;
+                }
+
+                return value;
+            }
+
+            return null;
+        }
+    }
+}
