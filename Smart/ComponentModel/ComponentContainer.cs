6b059f226296f1fda8552ee632b63a1e3171d8c7
usausa <machi.pon@gmail.com>
2016-09-03 21:11:59 +0900

    Update version to 0.5.0
    Add ComponentContainer

---
+++ Smart/ComponentModel/ComponentContainer.cs
@@ -0,0 +1,75 @@
+﻿namespace Smart.ComponentModel
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Smart.ComponentModel;
+
+    /// <summary>
+    ///
+    /// </summary>
+    public class ComponentContainer : DisposableObject, IComponentContainer
+    {
+        private readonly Dictionary<Type, object> components = new Dictionary<Type, object>();
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                foreach (var instance in components.Values)
+                {
+                    (instance as IDisposable)?.Dispose();
+                }
+
+                components.Clear();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="component"></param>
+        public void Register<T>(T component)
+        {
+            components[typeof(T)] = component;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        public void Remove<T>()
+        {
+            components.Remove(typeof(T));
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public T Get<T>()
+        {
+            object value;
+            return (T)(components.TryGetValue(typeof(T), out value) ? value : null);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public object Get(Type type)
+        {
+            object value;
+            return components.TryGetValue(type, out value) ? value : null;
+        }
+    }
+}
---
+++ Smart/ComponentModel/IComponentContainer.cs
@@ -0,0 +1,26 @@
+﻿namespace Smart.ComponentModel
+{
+    using System;
+
+    /// <summary>
+    ///
+    /// </summary>
+    public interface IComponentContainer
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1716:IdentifiersShouldNotMatchKeywords", Justification = "Ignore")]
+        T Get<T>();
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1716:IdentifiersShouldNotMatchKeywords", Justification = "Ignore")]
+        object Get(Type type);
+    }
+}
