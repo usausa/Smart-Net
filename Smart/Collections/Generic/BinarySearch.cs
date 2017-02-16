7e9b6f64debdd1281ef1ce3d4eda36e60a531d24
usausa <machi.pon@gmail.com>
2017-02-16 13:45:35 +0900

    Update BinarySearch

--- Smart/Collections/Generic/BinarySearch.cs
+++ Smart/Collections/Generic/BinarySearch.cs
@@ -168,8 +168,21 @@
         }
 
         //--------------------------------------------------------------------------------
-        // IComparer version
+        // IComparer version Find
         //--------------------------------------------------------------------------------
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Ignore")]
+        public static int Find<T>(IList<T> list, T key)
+        {
+            return Find(list, 0, list.Count, key, Functions<T>.Identify, Comparer<T>.Default);
+        }
 
         /// <summary>
         ///
@@ -193,12 +206,72 @@
         /// <param name="list"></param>
         /// <param name="key"></param>
         /// <param name="selector"></param>
+        /// <returns></returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Ignore")]
+        public static int Find<T, TKey>(IList<T> list, TKey key, Func<T, TKey> selector)
+        {
+            return Find(list, 0, list.Count, key, selector, Comparer<TKey>.Default);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="key"></param>
+        /// <param name="selector"></param>
         /// <param name="comparer"></param>
         /// <returns></returns>
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Ignore")]
         public static int Find<T, TKey>(IList<T> list, TKey key, Func<T, TKey> selector, IComparer<TKey> comparer)
         {
             return Find(list, 0, list.Count, key, selector, comparer);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="index"></param>
+        /// <param name="length"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static int Find<T>(IList<T> list, int index, int length, T key)
+        {
+            return Find(list, index, length, key, Functions<T>.Identify, Comparer<T>.Default);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="index"></param>
+        /// <param name="length"></param>
+        /// <param name="key"></param>
+        /// <param name="comparer"></param>
+        /// <returns></returns>
+        public static int Find<T>(IList<T> list, int index, int length, T key, IComparer<T> comparer)
+        {
+            return Find(list, index, length, key, Functions<T>.Identify, comparer);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="index"></param>
+        /// <param name="length"></param>
+        /// <param name="key"></param>
+        /// <param name="selector"></param>
+        /// <returns></returns>
+        public static int Find<T, TKey>(IList<T> list, int index, int length, TKey key, Func<T, TKey> selector)
+        {
+            return Find(list, index, length, key, selector, Comparer<TKey>.Default);
         }
 
         /// <summary>
@@ -242,6 +315,23 @@
             return ~lo;
         }
 
+        //--------------------------------------------------------------------------------
+        // IComparer version FindFirst
+        //--------------------------------------------------------------------------------
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Ignore")]
+        public static int FindFirst<T>(IList<T> list, T key)
+        {
+            return FindFirst(list, 0, list.Count, key, Functions<T>.Identify, Comparer<T>.Default);
+        }
+
         /// <summary>
         ///
         /// </summary>
@@ -264,12 +354,72 @@
         /// <param name="list"></param>
         /// <param name="key"></param>
         /// <param name="selector"></param>
+        /// <returns></returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Ignore")]
+        public static int FindFirst<T, TKey>(IList<T> list, TKey key, Func<T, TKey> selector)
+        {
+            return FindFirst(list, 0, list.Count, key, selector, Comparer<TKey>.Default);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="key"></param>
+        /// <param name="selector"></param>
         /// <param name="comparer"></param>
         /// <returns></returns>
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Ignore")]
         public static int FindFirst<T, TKey>(IList<T> list, TKey key, Func<T, TKey> selector, IComparer<TKey> comparer)
         {
             return FindFirst(list, 0, list.Count, key, selector, comparer);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="index"></param>
+        /// <param name="length"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static int FindFirst<T>(IList<T> list, int index, int length, T key)
+        {
+            return FindFirst(list, index, length, key, Functions<T>.Identify, Comparer<T>.Default);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="index"></param>
+        /// <param name="length"></param>
+        /// <param name="key"></param>
+        /// <param name="comparer"></param>
+        /// <returns></returns>
+        public static int FindFirst<T>(IList<T> list, int index, int length, T key, IComparer<T> comparer)
+        {
+            return FindFirst(list, index, length, key, Functions<T>.Identify, comparer);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="index"></param>
+        /// <param name="length"></param>
+        /// <param name="key"></param>
+        /// <param name="selector"></param>
+        /// <returns></returns>
+        public static int FindFirst<T, TKey>(IList<T> list, int index, int length, TKey key, Func<T, TKey> selector)
+        {
+            return FindFirst(list, index, length, key, selector, Comparer<TKey>.Default);
         }
 
         /// <summary>
@@ -314,6 +464,23 @@
             return find >= 0 ? find : ~lo;
         }
 
+        //--------------------------------------------------------------------------------
+        // IComparer version FindLast
+        //--------------------------------------------------------------------------------
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Ignore")]
+        public static int FindLast<T>(IList<T> list, T key)
+        {
+            return FindLast(list, 0, list.Count, key, Functions<T>.Identify, Comparer<T>.Default);
+        }
+
         /// <summary>
         ///
         /// </summary>
@@ -336,12 +503,72 @@
         /// <param name="list"></param>
         /// <param name="key"></param>
         /// <param name="selector"></param>
+        /// <returns></returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Ignore")]
+        public static int FindLast<T, TKey>(IList<T> list, TKey key, Func<T, TKey> selector)
+        {
+            return FindLast(list, 0, list.Count, key, selector, Comparer<TKey>.Default);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="key"></param>
+        /// <param name="selector"></param>
         /// <param name="comparer"></param>
         /// <returns></returns>
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Ignore")]
         public static int FindLast<T, TKey>(IList<T> list, TKey key, Func<T, TKey> selector, IComparer<TKey> comparer)
         {
             return FindLast(list, 0, list.Count, key, selector, comparer);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="index"></param>
+        /// <param name="length"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static int FindLast<T>(IList<T> list, int index, int length, T key)
+        {
+            return FindLast(list, index, length, key, Functions<T>.Identify, Comparer<T>.Default);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="index"></param>
+        /// <param name="length"></param>
+        /// <param name="key"></param>
+        /// <param name="comparer"></param>
+        /// <returns></returns>
+        public static int FindLast<T>(IList<T> list, int index, int length, T key, IComparer<T> comparer)
+        {
+            return FindLast(list, index, length, key, Functions<T>.Identify, comparer);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="index"></param>
+        /// <param name="length"></param>
+        /// <param name="key"></param>
+        /// <param name="selector"></param>
+        /// <returns></returns>
+        public static int FindLast<T, TKey>(IList<T> list, int index, int length, TKey key, Func<T, TKey> selector)
+        {
+            return FindLast(list, index, length, key, selector, Comparer<TKey>.Default);
         }
 
         /// <summary>
