19a50bc08fdaec5f9b512eff90811df1c4942b81
usausa <machi.pon@gmail.com>
2017-04-24 10:13:32 +0900

    Add EmptyTask

--- Smart/Functional/ObjectExtensions.cs
+++ Smart/Functional/ObjectExtensions.cs
@@ -1,6 +1,7 @@
 ﻿namespace Smart.Functional
 {
     using System;
+    using System.Threading.Tasks;
 
     /// <summary>
     ///
@@ -61,6 +62,20 @@
             where T : struct
         {
             return value == null ? default(TResult) : func(value.Value);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="value"></param>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Ignore")]
+        public static Task<TResult> Or<T, TResult>(this T value, Func<T, Task<TResult>> func)
+        {
+            return value == null ? Empty<TResult>.Task : func(value);
         }
     }
 }
