6816a3898fa9c2c365454230dc92fef1a2da5fef
usausa <machi.pon@gmail.com>
2017-05-01 17:40:27 +0900

    Update ArrayExtensionTest

--- Smart.Tests.NETCore/ArrayExtensionTest.Remove.cs
+++ Smart.Tests.NETCore/ArrayExtensionTest.Remove.cs
@@ -6,11 +6,24 @@
     {
         [Fact]
 
+        public static void TestArrayRemoveAt()
+        {
+            Assert.Equal(null, ((int[])null).RemoveAt(0));
+            Assert.Equal(new[] { 2, 3 }, new[] { 1, 2, 3 }.RemoveAt(0));
+            Assert.Equal(new[] { 1, 3 }, new[] { 1, 2, 3 }.RemoveAt(1));
+            Assert.Equal(new[] { 1, 2 }, new[] { 1, 2, 3 }.RemoveAt(2));
+            Assert.Equal(new[] { 1, 2, 3 }, new[] { 1, 2, 3 }.RemoveAt(3));
+        }
+
+        [Fact]
+
         public static void TestArrayRemoveRange()
         {
-            var array = new[] { 1, 2, 3, 4 };
-
-            Assert.Equal(new[] { 1, 4 }, array.RemoveRange(1, 2));
+            Assert.Equal(null, ((int[])null).RemoveRange(0, 1));
+            Assert.Equal(new[] { 3, 4 }, new[] { 1, 2, 3, 4 }.RemoveRange(0, 2));
+            Assert.Equal(new[] { 1, 4 }, new[] { 1, 2, 3, 4 }.RemoveRange(1, 2));
+            Assert.Equal(new[] { 1, 2 }, new[] { 1, 2, 3, 4 }.RemoveRange(2, 2));
+            Assert.Equal(new[] { 1, 2, 3 }, new[] { 1, 2, 3, 4 }.RemoveRange(3, 2));
         }
     }
 }
