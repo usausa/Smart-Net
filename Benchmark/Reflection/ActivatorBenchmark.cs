8432425d063b8357ba42976c0a197ba67cd53107
usausa <machi.pon@gmail.com>
2017-09-14 10:35:17 +0900

    Update benchmark

--- Benchmark/Reflection/ActivatorBenchmark.cs
+++ Benchmark/Reflection/ActivatorBenchmark.cs
@@ -1,6 +1,7 @@
 ﻿namespace Benchmark.Reflection
 {
     using System;
+    using System.Reflection;
 
     using BenchmarkDotNet.Attributes;
 
@@ -12,6 +13,18 @@
     [Config(typeof(BenchmarkConfig))]
     public class ActivatorBenchmark
     {
+        public sealed class NewActivator : IActivator
+        {
+            public ConstructorInfo Source { get; } = typeof(Class0).GetConstructor(Type.EmptyTypes);
+
+            public object Create(params object[] arguments)
+            {
+                return new Class0();
+            }
+        }
+
+        private IActivator newActivator;
+
         private IActivator codeGenerateActivator;
 
         private IActivator reflectionActivator;
@@ -20,14 +33,21 @@
         public void Setup()
         {
             var ctor = typeof(Class0).GetConstructor(Type.EmptyTypes);
+            newActivator = new NewActivator();
             codeGenerateActivator = CodeGenerateTypeMetadataFactory.Default.CreateActivator(ctor);
             reflectionActivator = ReflectionTypeMetadataFactory.Default.CreateActivator(ctor);
         }
 
         [Benchmark]
-        public object New()
+        public object NewRaw()
         {
             return new Class0();
+        }
+
+        [Benchmark]
+        public object NewWithActivator()
+        {
+            return newActivator.Create(null);
         }
 
         [Benchmark]
