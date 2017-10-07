019b0958b6a81744b8bcdf654e3d776565477887
usausa <machi.pon@gmail.com>
2017-10-07 20:40:02 +0900

    Add ILGeneratorExtensions

---
+++ Smart/Reflection/Emit/ILGeneratorExtensions.cs
@@ -0,0 +1,77 @@
+﻿namespace Smart.Reflection.Emit
+{
+    using System;
+    using System.Reflection.Emit;
+
+    /// <summary>
+    ///
+    /// </summary>
+    public static class ILGeneratorExtensions
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="il"></param>
+        /// <param name="i"></param>
+        public static void EmitLdcI4(ILGenerator il, int i)
+        {
+            switch (i)
+            {
+                case 0:
+                    il.Emit(OpCodes.Ldc_I4_0);
+                    break;
+                case 1:
+                    il.Emit(OpCodes.Ldc_I4_1);
+                    break;
+                case 2:
+                    il.Emit(OpCodes.Ldc_I4_2);
+                    break;
+                case 3:
+                    il.Emit(OpCodes.Ldc_I4_3);
+                    break;
+                case 4:
+                    il.Emit(OpCodes.Ldc_I4_4);
+                    break;
+                case 5:
+                    il.Emit(OpCodes.Ldc_I4_5);
+                    break;
+                case 6:
+                    il.Emit(OpCodes.Ldc_I4_6);
+                    break;
+                case 7:
+                    il.Emit(OpCodes.Ldc_I4_7);
+                    break;
+                case 8:
+                    il.Emit(OpCodes.Ldc_I4_8);
+                    break;
+                default:
+                    if (i < 128)
+                    {
+                        il.Emit(OpCodes.Ldc_I4_S, (sbyte)i);
+                    }
+                    else
+                    {
+                        il.Emit(OpCodes.Ldc_I4, i);
+                    }
+                    break;
+            }
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="il"></param>
+        /// <param name="type"></param>
+        public static void EmitTypeConversion(ILGenerator il, Type type)
+        {
+            if (type.IsValueType)
+            {
+                il.Emit(OpCodes.Unbox_Any, type);
+            }
+            else if (type != typeof(object))
+            {
+                il.Emit(OpCodes.Castclass, type);
+            }
+        }
+    }
+}
