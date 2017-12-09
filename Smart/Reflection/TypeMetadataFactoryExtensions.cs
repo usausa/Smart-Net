36c354ef3bbc11ee478038bce855ba252f328e62
usausa <machi.pon@gmail.com>
2017-12-09 15:07:52 +0900

    Fix TypeMetadataFactoryExtensions

--- Smart/Reflection/TypeMetadataFactoryExtensions.cs
+++ Smart/Reflection/TypeMetadataFactoryExtensions.cs
@@ -4,13 +4,13 @@
     {
         public static void ConfigurePerformance(this TypeMetadataFactory factory)
         {
-            factory.AccessorFactory = EmitTypeMetadataFactory.Default;
+            factory.ActivatorFactory = EmitTypeMetadataFactory.Default;
             factory.AccessorFactory = EmitTypeMetadataFactory.Default;
         }
 
         public static void ConfigureSafe(this TypeMetadataFactory factory)
         {
-            factory.AccessorFactory = ReflectionTypeMetadataFactory.Default;
+            factory.ActivatorFactory = ReflectionTypeMetadataFactory.Default;
             factory.AccessorFactory = ReflectionTypeMetadataFactory.Default;
         }
     }
