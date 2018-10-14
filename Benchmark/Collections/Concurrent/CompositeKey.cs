ff3030fdb6c62b4f8e7dcb469a8b5f0d6b019408
usausa <machi.pon@gmail.com>
2018-10-14 10:39:08 +0900

    Update benchmark

---
+++ Benchmark/Collections/Concurrent/CompositeKey.cs
@@ -0,0 +1,23 @@
+﻿namespace Benchmark.Collections.Concurrent
+{
+    using System;
+
+    public struct CompositeKey : IEquatable<CompositeKey>
+    {
+        public Type Type { get; }
+
+        public string Name { get; }
+
+        public CompositeKey(Type type, string name)
+        {
+            Type = type;
+            Name = name;
+        }
+
+        public bool Equals(CompositeKey other) => Type == other.Type && Name == other.Name;
+
+        public override bool Equals(object obj) => obj is CompositeKey other && Equals(other);
+
+        public override int GetHashCode() => (Type, Name).GetHashCode();
+    }
+}
--- Benchmark/Collections/Concurrent/DictionaryBenchmark.cs
+++ Benchmark/Collections/Concurrent/DictionaryBenchmark.cs
@@ -20,7 +20,15 @@
 
         private readonly ThreadsafeTypeHashArrayMap<object> hashArrayMap = new ThreadsafeTypeHashArrayMap<object>();
 
+        private readonly Dictionary<CompositeKey, object> compositeDictionary = new Dictionary<CompositeKey, object>();
+
+        private readonly ConcurrentDictionary<CompositeKey, object> compositeConcurrentDictionary = new ConcurrentDictionary<CompositeKey, object>();
+
+        private readonly ThreadsafeHashArrayMap<CompositeKey, object> compositeHashArrayMap = new ThreadsafeHashArrayMap<CompositeKey, object>();
+
         private readonly Type key = typeof(Class0);
+
+        private readonly CompositeKey compositeKey = new CompositeKey(typeof(Class0), string.Empty);
 
         [GlobalSetup]
         public void Setup()
@@ -30,6 +38,11 @@
                 dictionary[type] = type;
                 concurrentDictionary[type] = type;
                 hashArrayMap.AddIfNotExist(type, type);
+
+                var ck = new CompositeKey(type, string.Empty);
+                compositeDictionary[ck] = type;
+                compositeConcurrentDictionary[ck] = type;
+                compositeHashArrayMap.AddIfNotExist(ck, type);
             }
         }
 
@@ -52,6 +65,27 @@
         public object ThreadsafeTypeHashArrayMap()
         {
             return hashArrayMap.TryGetValue(key, out var obj) ? obj : null;
+        }
+
+        [Benchmark]
+        public object CompositeDictionary()
+        {
+            lock (compositeDictionary)
+            {
+                return compositeDictionary.TryGetValue(compositeKey, out var obj) ? obj : null;
+            }
+        }
+
+        [Benchmark]
+        public object CompositeConcurrentDictionary()
+        {
+            return compositeConcurrentDictionary.TryGetValue(compositeKey, out var obj) ? obj : null;
+        }
+
+        [Benchmark]
+        public object CompositeThreadsafeTypeHashArrayMap()
+        {
+            return compositeHashArrayMap.TryGetValue(compositeKey, out var obj) ? obj : null;
         }
     }
 }
