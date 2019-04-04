2c4e4540766b05456dd8261748c9688764d06d89
usausa <machi.pon@gmail.com>
2019-04-04 16:05:51 +0900

    Update inspection

--- Smart/ComponentModel/IComponentContainer.cs
+++ Smart/ComponentModel/IComponentContainer.cs
@@ -3,16 +3,15 @@
     using System;
     using System.Collections.Generic;
 
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1716:IdentifiersShouldNotMatchKeywords", Justification = "Ignore")]
     public interface IComponentContainer : IDisposable, IServiceProvider
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1716:IdentifiersShouldNotMatchKeywords", Justification = "Ignore")]
         T Get<T>();
 
         T TryGet<T>();
 
         IEnumerable<T> GetAll<T>();
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1716:IdentifiersShouldNotMatchKeywords", Justification = "Ignore")]
         object Get(Type componentType);
 
         object TryGet(Type componentType);
