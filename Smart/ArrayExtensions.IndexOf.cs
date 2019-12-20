ce8398ff1956df0ff5fcaa52a71c901a2ce27abd
usausa <machi.pon@gmail.com>
2019-12-20 23:32:52 +0900

    Update index functions

--- Smart/ArrayExtensions.IndexOf.cs
+++ Smart/ArrayExtensions.IndexOf.cs
@@ -1,4 +1,4 @@
-﻿namespace Smart
+namespace Smart
 {
     using System;
 
@@ -26,16 +26,94 @@
             return -1;
         }
 
-        public static int IndexOf<T>(this T[] array, int offset, int length, Func<T, bool> predicate)
+        public static int IndexOf<T>(this T[] array, int start, Func<T, bool> predicate)
         {
             if (array is null)
             {
                 return -1;
             }
 
-            var end = offset + length;
+            for (var i = start; i < array.Length; i++)
+            {
+                if (predicate(array[i]))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public static int IndexOf<T>(this T[] array, int start, int count, Func<T, bool> predicate)
+        {
+            if (array is null)
+            {
+                return -1;
+            }
+
+            var end = start + count;
             var limit = end > array.Length ? array.Length : end;
-            for (var i = offset; i < limit; i++)
+            for (var i = start; i < limit; i++)
+            {
+                if (predicate(array[i]))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        //--------------------------------------------------------------------------------
+        // LastIndexOf
+        //--------------------------------------------------------------------------------
+
+        public static int LastIndexOf<T>(this T[] array, Func<T, bool> predicate)
+        {
+            if (array is null)
+            {
+                return -1;
+            }
+
+            for (var i = array.Length - 1; i >= 0; i--)
+            {
+                if (predicate(array[i]))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public static int LastIndexOf<T>(this T[] array, int start, Func<T, bool> predicate)
+        {
+            if (array is null)
+            {
+                return -1;
+            }
+
+            for (var i = start; i >= 0; i--)
+            {
+                if (predicate(array[i]))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public static int LastIndexOf<T>(this T[] array, int start, int count, Func<T, bool> predicate)
+        {
+            if (array is null)
+            {
+                return -1;
+            }
+
+            var end = start + 1 - count;
+            var limit = end < 0 ? 0 : end;
+            for (var i = start; i >= limit; i--)
             {
                 if (predicate(array[i]))
                 {
