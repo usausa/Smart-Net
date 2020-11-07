8a0147381571cd6eefdca33045ee1d29c7216304
usausa <machi.pon@gmail.com>
2020-11-07 14:34:54 +0900

    Update notification object

--- Smart/ComponentModel/NotificationObject.cs
+++ Smart/ComponentModel/NotificationObject.cs
@@ -1,11 +1,20 @@
-﻿namespace Smart.ComponentModel
+namespace Smart.ComponentModel
 {
+    using System.Collections.Generic;
     using System.ComponentModel;
     using System.Runtime.CompilerServices;
 
-    public abstract class NotificationObject : INotifyPropertyChanged
+    public abstract class NotificationObject : INotifyPropertyChanged, INotifyPropertyChanging
     {
+        public event PropertyChangingEventHandler PropertyChanging;
+
         public event PropertyChangedEventHandler PropertyChanged;
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1030:UseEventsWhereAppropriate", Justification = "Ignore")]
+        protected void RaisePropertyChanging([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+        }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1030:UseEventsWhereAppropriate", Justification = "Ignore")]
         protected void RaisePropertyChanged([CallerMemberName] string propertyName = null)
@@ -15,11 +24,26 @@
 
         protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
         {
-            if (Equals(storage, value))
+            if (EqualityComparer<T>.Default.Equals(storage, value))
             {
                 return false;
             }
 
+            RaisePropertyChanging(propertyName);
+            storage = value;
+            RaisePropertyChanged(propertyName);
+
+            return true;
+        }
+
+        protected bool SetProperty<T>(ref T storage, T value, IEqualityComparer<T> comparer, [CallerMemberName] string propertyName = null)
+        {
+            if (comparer.Equals(storage, value))
+            {
+                return false;
+            }
+
+            RaisePropertyChanging(propertyName);
             storage = value;
             RaisePropertyChanged(propertyName);
 
