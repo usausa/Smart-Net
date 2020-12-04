ab76e0a08104f28ee5a84619258c7fea765b4ad5
usausa <machi.pon@gmail.com>
2020-12-04 16:35:31 +0900

    Update enumerable extensions

--- Smart/Linq/Enumerable.Indexed.cs
+++ Smart/Linq/Enumerable.Indexed.cs
@@ -3,122 +3,11 @@ namespace Smart.Linq
     using System;
     using System.Collections;
     using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
 
     public static partial class EnumerableExtensions
     {
-        //--------------------------------------------------------------------------------
-        // Indexed.Array
-        //--------------------------------------------------------------------------------
-
-        private struct IndexedArrayEnumerator<T> : IEnumerator<Indexed<T>>
-        {
-            private readonly T[] array;
-
-            private int index;
-
-            internal IndexedArrayEnumerator(T[] array)
-            {
-                this.array = array;
-                index = -1;
-            }
-
-            public void Dispose()
-            {
-            }
-
-            public Indexed<T> Current => new Indexed<T>(array[index], index);
-
-            object IEnumerator.Current => Current;
-
-            public bool MoveNext()
-            {
-                index++;
-                return index < array.Length;
-            }
-
-            public void Reset() => throw new NotSupportedException();
-        }
-
-        private readonly struct IndexedArrayEnumerable<T> : IEnumerable<Indexed<T>>
-        {
-            private readonly T[] array;
-
-            public IndexedArrayEnumerable(T[] array)
-            {
-                this.array = array;
-            }
-
-            IEnumerator<Indexed<T>> IEnumerable<Indexed<T>>.GetEnumerator() => GetEnumerator();
-
-            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-            private IndexedArrayEnumerator<T> GetEnumerator() => new IndexedArrayEnumerator<T>(array);
-        }
-
-        public static IEnumerable<Indexed<T>> Indexed<T>(this T[] source)
-        {
-            return new IndexedArrayEnumerable<T>(source);
-        }
-
-        //--------------------------------------------------------------------------------
-        // Indexed.IList
-        //--------------------------------------------------------------------------------
-
-        private struct IndexedListEnumerator<T> : IEnumerator<Indexed<T>>
-        {
-            private readonly IList<T> list;
-
-            private int index;
-
-            internal IndexedListEnumerator(IList<T> list)
-            {
-                this.list = list;
-                index = -1;
-            }
-
-            public void Dispose()
-            {
-            }
-
-            public Indexed<T> Current => new Indexed<T>(list[index], index);
-
-            object IEnumerator.Current => Current;
-
-            public bool MoveNext()
-            {
-                index++;
-                return index < list.Count;
-            }
-
-            public void Reset() => throw new NotSupportedException();
-        }
-
-        private readonly struct IndexedListEnumerable<T> : IEnumerable<Indexed<T>>
-        {
-            private readonly IList<T> list;
-
-            public IndexedListEnumerable(IList<T> list)
-            {
-                this.list = list;
-            }
-
-            IEnumerator<Indexed<T>> IEnumerable<Indexed<T>>.GetEnumerator() => GetEnumerator();
-
-            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-            private IndexedListEnumerator<T> GetEnumerator() => new IndexedListEnumerator<T>(list);
-        }
-
-        public static IEnumerable<Indexed<T>> Indexed<T>(this IList<T> source)
-        {
-            return new IndexedListEnumerable<T>(source);
-        }
-
-        //--------------------------------------------------------------------------------
-        // Indexed
-        //--------------------------------------------------------------------------------
-
-        private struct IndexedEnumerator<T> : IEnumerator<Indexed<T>>
+        private sealed class IndexedEnumerator<T> : IEnumerator<Indexed<T>>
         {
             private readonly IEnumerator<T> ie;
 
@@ -132,6 +21,7 @@ namespace Smart.Linq
 
             public void Dispose()
             {
+                ie.Dispose();
             }
 
             public Indexed<T> Current => new Indexed<T>(ie.Current, index);
@@ -156,25 +46,16 @@ namespace Smart.Linq
                 this.ie = ie;
             }
 
-            IEnumerator<Indexed<T>> IEnumerable<Indexed<T>>.GetEnumerator() => GetEnumerator();
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            IEnumerator<Indexed<T>> IEnumerable<Indexed<T>>.GetEnumerator() => new IndexedEnumerator<T>(ie.GetEnumerator());
 
-            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-            private IndexedEnumerator<T> GetEnumerator() => new IndexedEnumerator<T>(ie.GetEnumerator());
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            IEnumerator IEnumerable.GetEnumerator() => new IndexedEnumerator<T>(ie.GetEnumerator());
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IEnumerable<Indexed<T>> Indexed<T>(this IEnumerable<T> source)
         {
-            if (source is T[] array)
-            {
-                return new IndexedArrayEnumerable<T>(array);
-            }
-
-            if (source is IList<T> list)
-            {
-                return new IndexedListEnumerable<T>(list);
-            }
-
             return new IndexedEnumerable<T>(source);
         }
     }
