0731eda4043ec135d31d55ab361e882b181a957e
usausa <machi.pon@gmail.com>
2020-12-16 13:05:30 +0900

    Update extensions

--- Smart/Functional/ObjectExtensions.cs
+++ Smart/Functional/ObjectExtensions.cs
@@ -9,6 +9,27 @@ namespace Smart.Functional
     public static class ObjectExtensions
     {
         //--------------------------------------------------------------------------------
+        // If
+        //--------------------------------------------------------------------------------
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void IfNotNull<T>(this T? value, Action<T> action)
+        {
+            if (value is null)
+            {
+                return;
+            }
+
+            action(value);
+        }
+
+        // Async
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Task IfNotNullAsync<T>(this T? value, Func<T, Task> action) =>
+            value is null ? Task.CompletedTask : action(value);
+
+        //--------------------------------------------------------------------------------
         // Also
         //--------------------------------------------------------------------------------
 
@@ -26,18 +47,37 @@ namespace Smart.Functional
             {
                 action(value);
             }
+
             return value;
         }
+
+        // Async
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Task<T> AlsoAsync<T>(this T value, Func<T, Task<T>> action) =>
+            action(value);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Task<T> AlsoIfAsync<T>(this T value, Func<T, bool> predicate, Func<T, Task<T>> action) =>
+            predicate(value) ? action(value) : Task.FromResult(value);
+
+        // Async
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ValueTask<T> AlsoAsync<T>(this T value, Func<T, ValueTask<T>> action) =>
+            action(value);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ValueTask<T> AlsoIfAsync<T>(this T value, Func<T, bool> predicate, Func<T, ValueTask<T>> action) =>
+            predicate(value) ? action(value) : new ValueTask<T>(value);
 
         //--------------------------------------------------------------------------------
         // Apply
         //--------------------------------------------------------------------------------
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void Apply<T>(this T value, Action<T> action)
-        {
+        public static void Apply<T>(this T value, Action<T> action) =>
             action(value);
-        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void ApplyIf<T>(this T value, Func<T, bool> predicate, Action<T> action)
@@ -48,154 +88,103 @@ namespace Smart.Functional
             }
         }
 
-        // TODO
+        // Async
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void IfNotNull<T>(this T value, Action<T> action)
-            where T : class
-        {
-            if (value is null)
-            {
-                return;
-            }
-
+        public static Task ApplyAsync<T>(this T value, Func<T, Task> action) =>
             action(value);
-        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void IfNotNull<T>(this T? value, Action<T> action)
-            where T : struct
-        {
-            if (value is null)
-            {
-                return;
-            }
+        public static Task ApplyIfAsync<T>(this T value, Func<T, bool> predicate, Func<T, Task> action) =>
+            predicate(value) ? action(value) : Task.CompletedTask;
 
-            action(value.Value);
-        }
+        // Async
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ValueTask ApplyAsync<T>(this T value, Func<T, ValueTask> action) =>
+            action(value);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ValueTask ApplyIfAsync<T>(this T value, Func<T, bool> predicate, Func<T, ValueTask> action) =>
+            predicate(value) ? action(value) : default;
 
         //--------------------------------------------------------------------------------
         // Map
         //--------------------------------------------------------------------------------
 
-        // TODO
+        public static TResult? Map<T, TResult>(this T? value, Func<T?, TResult?> func) =>
+            func(value);
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static TResult Apply<T, TResult>(this T value, Func<T, TResult> func)
-        {
-            return func(value);
-        }
+        public static TResult? MapOrDefault<T, TResult>(this T? value, Func<T, TResult?> func) =>
+            value is null ? default : func(value);
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static TResult ApplyIfOrDefault<T, TResult>(this T value, Func<T, bool> predicate, Func<T, TResult> func)
-        {
-            return predicate(value) ? func(value) : default;
-        }
+        public static TResult? MapOrDefault<T, TResult>(this T? value, Func<T, TResult?> func, TResult? defaultValue) =>
+            value is null ? defaultValue : func(value);
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static TResult ApplyIf<T, TResult>(this T value, Func<T, bool> predicate, Func<T, TResult> func, TResult defaultValue)
-        {
-            return predicate(value) ? func(value) : defaultValue;
-        }
+        public static TResult? MapIfOrDefault<T, TResult>(this T? value, Func<T?, bool> predicate, Func<T?, TResult?> func) =>
+            predicate(value) ? func(value) : default;
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static TResult ApplyIf<T, TResult>(this T value, Func<T, bool> predicate, Func<T, TResult> func, Func<TResult> defaultValueFactory)
-        {
-            return predicate(value) ? func(value) : defaultValueFactory();
-        }
+        public static TResult? MapIfOrDefault<T, TResult>(this T? value, Func<T?, bool> predicate, Func<T?, TResult?> func, TResult? defaultValue) =>
+            predicate(value) ? func(value) : defaultValue;
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static TResult MapOrDefault<T, TResult>(this T value, Func<T, TResult> func)
-            where T : class
-        {
-            return value is null ? default : func(value);
-        }
+        // Async
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static TResult MapOr<T, TResult>(this T value, Func<T, TResult> func, TResult defaultValue)
-            where T : class
-        {
-            return value is null ? defaultValue : func(value);
-        }
+        public static Task<TResult?> MapAsync<T, TResult>(this T? value, Func<T?, Task<TResult?>> func) =>
+            func(value);
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static TResult MapOr<T, TResult>(this T value, Func<T, TResult> func, Func<TResult> defaultValueFactory)
-            where T : class
-        {
-            return value is null ? defaultValueFactory() : func(value);
-        }
+        public static Task<TResult?> MapOrDefault<T, TResult>(this T? value, Func<T, Task<TResult?>> func) =>
+            value is null ? Task.FromResult(default(TResult)) : func(value);
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static TResult MapOrDefault<T, TResult>(this T? value, Func<T, TResult> func)
-            where T : struct
-        {
-            return value is null ? default : func(value.Value);
-        }
+        public static Task<TResult?> MapOrDefault<T, TResult>(this T? value, Func<T, Task<TResult?>> func, TResult? defaultValue) =>
+            value is null ? Task.FromResult(defaultValue) : func(value);
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static TResult MapOr<T, TResult>(this T? value, Func<T, TResult> func, TResult defaultValue)
-            where T : struct
-        {
-            return value is null ? defaultValue : func(value.Value);
-        }
+        public static Task<TResult?> MapIfOrDefault<T, TResult>(this T? value, Func<T?, bool> predicate, Func<T?, Task<TResult?>> func) =>
+            predicate(value) ? func(value) : Default<TResult>.FromResultTask;
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static TResult MapOr<T, TResult>(this T? value, Func<T, TResult> func, Func<TResult> defaultValueFactory)
-            where T : struct
-        {
-            return value is null ? defaultValueFactory() : func(value.Value);
-        }
+        public static Task<TResult?> MapIfOrDefault<T, TResult>(this T? value, Func<T?, bool> predicate, Func<T?, Task<TResult?>> func, TResult? defaultValue) =>
+            predicate(value) ? func(value) : Task.FromResult(defaultValue);
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Task<TResult> MapOrDefaultAsync<T, TResult>(this T value, Func<T, Task<TResult>> func)
-        {
-            return value is null ? Default<TResult>.TaskFromResult : func(value);
-        }
+        // Async
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Task<TResult> MapOrAsync<T, TResult>(this T value, Func<T, Task<TResult>> func, Task<TResult> defaultTask)
-        {
-            return value is null ? defaultTask : func(value);
-        }
+        public static ValueTask<TResult?> MapAsync<T, TResult>(this T? value, Func<T?, ValueTask<TResult?>> func) =>
+            func(value);
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Task<TResult> MapOrAsync<T, TResult>(this T value, Func<T, Task<TResult>> func, Func<Task<TResult>> defaultTaskFactory)
-        {
-            return value is null ? defaultTaskFactory() : func(value);
-        }
+        public static ValueTask<TResult?> MapOrDefault<T, TResult>(this T? value, Func<T, ValueTask<TResult?>> func) =>
+            value is null ? new ValueTask<TResult?>(default(TResult)) : func(value);
+
+        public static ValueTask<TResult?> MapOrDefault<T, TResult>(this T? value, Func<T, ValueTask<TResult?>> func, TResult? defaultValue) =>
+            value is null ? new ValueTask<TResult?>(defaultValue) : func(value);
+
+        public static ValueTask<TResult?> MapIfOrDefault<T, TResult>(this T? value, Func<T?, bool> predicate, Func<T?, ValueTask<TResult?>> func) =>
+            predicate(value) ? func(value) : new ValueTask<TResult?>(default(TResult));
+
+        public static ValueTask<TResult?> MapIfOrDefault<T, TResult>(this T? value, Func<T?, bool> predicate, Func<T?, ValueTask<TResult?>> func, TResult? defaultValue) =>
+            predicate(value) ? func(value) : new ValueTask<TResult?>(defaultValue);
 
         //--------------------------------------------------------------------------------
         // Enumerable
         //--------------------------------------------------------------------------------
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static IEnumerable<T> FlatOrEmpty<T>(this T value)
-            where T : class
-        {
-            return value is null ? Enumerable.Empty<T>() : FromSingleValue(value);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IEnumerable<T> FlatOrEmpty<T>(this T? value)
-            where T : struct
         {
-            return value is null ? Enumerable.Empty<T>() : FromSingleValue(value.Value);
+            return value is null ? Enumerable.Empty<T>() : Flat(value);
         }
 
-        private static IEnumerable<T> FromSingleValue<T>(T value)
+        private static IEnumerable<T> Flat<T>(T value)
         {
             yield return value;
         }
 
         //--------------------------------------------------------------------------------
-        // TODO
+        // Result
         //--------------------------------------------------------------------------------
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("ReSharper", "FieldCanBeMadeReadOnly.Local", Justification = "Ignore")]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401:FieldsMustBePrivate", Justification = "Ignore")]
         private static class Default<T>
         {
-            public static Task<T> TaskFromResult = Task.FromResult(default(T));
+            public static Task<T?> FromResultTask = Task.FromResult(default(T?));
         }
     }
 }
