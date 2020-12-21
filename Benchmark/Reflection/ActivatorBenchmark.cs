e549919f95daa4253f5a71c35afdf6e5a0cb6b30
usausa <machi.pon@gmail.com>
2020-12-21 20:42:55 +0900

    Update for nullable reference type

--- Benchmark/Reflection/ActivatorBenchmark.cs
+++ Benchmark/Reflection/ActivatorBenchmark.cs
@@ -14,23 +14,23 @@ namespace Benchmark.Reflection
 
         private static readonly object[] Parameter8 = { "x", "x", "x", "x", "x", "x", "x", "x" };
 
-        private Func<object[], object> newFactory0;
-        private Func<object[], object> dynamicFactory0A;
-        private Func<object[], object> reflectionFactory0A;
-        private Func<object> dynamicFactory0B;
-        private Func<object> reflectionFactory0B;
+        private Func<object?[]?, object>? newFactory0;
+        private Func<object?[]?, object>? dynamicFactory0A;
+        private Func<object?[]?, object>? reflectionFactory0A;
+        private Func<object>? dynamicFactory0B;
+        private Func<object>? reflectionFactory0B;
 
-        private Func<object[], object> newFactory1;
-        private Func<object[], object> dynamicFactory1A;
-        private Func<object[], object> reflectionFactory1A;
-        private Func<object, object> dynamicFactory1B;
-        private Func<object, object> reflectionFactory1B;
+        private Func<object?[]?, object>? newFactory1;
+        private Func<object?[]?, object>? dynamicFactory1A;
+        private Func<object?[]?, object>? reflectionFactory1A;
+        private Func<object?, object>? dynamicFactory1B;
+        private Func<object?, object>? reflectionFactory1B;
 
-        private Func<object[], object> newFactory8;
-        private Func<object[], object> dynamicFactory8A;
-        private Func<object[], object> reflectionFactory8A;
-        private Func<object, object, object, object, object, object, object, object, object> dynamicFactory8B;
-        private Func<object, object, object, object, object, object, object, object, object> reflectionFactory8B;
+        private Func<object?[]?, object>? newFactory8;
+        private Func<object?[]?, object>? dynamicFactory8A;
+        private Func<object?[]?, object>? reflectionFactory8A;
+        private Func<object?, object?, object?, object?, object?, object?, object?, object?, object>? dynamicFactory8B;
+        private Func<object?, object?, object?, object?, object?, object?, object?, object?, object>? reflectionFactory8B;
 
         [GlobalSetup]
         public void Setup()
@@ -45,13 +45,13 @@ namespace Benchmark.Reflection
             dynamicFactory0B = DynamicDelegateFactory.Default.CreateFactory0(ctor0);
             reflectionFactory0B = ReflectionDelegateFactory.Default.CreateFactory0(ctor0);
 
-            newFactory1 = args => new Data1((string)args[0]);
+            newFactory1 = args => new Data1((string?)args![0]);
             dynamicFactory1A = DynamicDelegateFactory.Default.CreateFactory(ctor1);
             reflectionFactory1A = ReflectionDelegateFactory.Default.CreateFactory(ctor1);
             dynamicFactory1B = DynamicDelegateFactory.Default.CreateFactory1(ctor1);
             reflectionFactory1B = ReflectionDelegateFactory.Default.CreateFactory1(ctor1);
 
-            newFactory8 = args => new Data8((string)args[0], (string)args[1], (string)args[2], (string)args[3], (string)args[4], (string)args[5], (string)args[6], (string)args[7]);
+            newFactory8 = args => new Data8((string?)args![0], (string?)args[1], (string?)args[2], (string?)args[3], (string?)args[4], (string?)args[5], (string?)args[6], (string?)args[7]);
             dynamicFactory8A = DynamicDelegateFactory.Default.CreateFactory(ctor8);
             reflectionFactory8A = ReflectionDelegateFactory.Default.CreateFactory(ctor8);
             dynamicFactory8B = DynamicDelegateFactory.Default.CreateFactory8(ctor8);
@@ -61,98 +61,53 @@ namespace Benchmark.Reflection
         // 0
 
         [Benchmark]
-        public object New()
-        {
-            return newFactory0(null);
-        }
+        public object New() => newFactory0!(null);
 
         [Benchmark]
-        public object Dynamic0A()
-        {
-            return dynamicFactory0A(null);
-        }
+        public object Dynamic0A() => dynamicFactory0A!(null);
 
         [Benchmark]
-        public object Dynamic0B()
-        {
-            return dynamicFactory0B();
-        }
+        public object Dynamic0B() => dynamicFactory0B!();
 
         [Benchmark]
-        public object Reflection0A()
-        {
-            return reflectionFactory0A(null);
-        }
+        public object Reflection0A() => reflectionFactory0A!(null);
 
         [Benchmark]
-        public object Reflection0B()
-        {
-            return reflectionFactory0B();
-        }
+        public object Reflection0B() => reflectionFactory0B!();
 
         // 1
 
         [Benchmark]
-        public object New1()
-        {
-            return newFactory1(Parameter1);
-        }
+        public object New1() => newFactory1!(Parameter1);
 
         [Benchmark]
-        public object Dynamic1A()
-        {
-            return dynamicFactory1A(Parameter1);
-        }
+        public object Dynamic1A() => dynamicFactory1A!(Parameter1);
 
         [Benchmark]
-        public object Dynamic1B()
-        {
-            return dynamicFactory1B("x");
-        }
+        public object Dynamic1B() => dynamicFactory1B!("x");
 
         [Benchmark]
-        public object Reflection1A()
-        {
-            return reflectionFactory1A(Parameter1);
-        }
+        public object Reflection1A() => reflectionFactory1A!(Parameter1);
 
         [Benchmark]
-        public object Reflection1B()
-        {
-            return reflectionFactory1B("x");
-        }
+        public object Reflection1B() => reflectionFactory1B!("x");
 
         // 8
 
         [Benchmark]
-        public object New8()
-        {
-            return newFactory8(Parameter8);
-        }
+        public object New8() => newFactory8!(Parameter8);
 
         [Benchmark]
-        public object Dynamic8A()
-        {
-            return dynamicFactory8A(Parameter8);
-        }
+        public object Dynamic8A() => dynamicFactory8A!(Parameter8);
 
         [Benchmark]
-        public object Dynamic8B()
-        {
-            return dynamicFactory8B("x", "x", "x", "x", "x", "x", "x", "x");
-        }
+        public object Dynamic8B() => dynamicFactory8B!("x", "x", "x", "x", "x", "x", "x", "x");
 
         [Benchmark]
-        public object Reflection8A()
-        {
-            return reflectionFactory8A(Parameter8);
-        }
+        public object Reflection8A() => reflectionFactory8A!(Parameter8);
 
         [Benchmark]
-        public object Reflection8B()
-        {
-            return reflectionFactory8B("x", "x", "x", "x", "x", "x", "x", "x");
-        }
+        public object Reflection8B() => reflectionFactory8B!("x", "x", "x", "x", "x", "x", "x", "x");
 
         // Data
 
@@ -162,9 +117,9 @@ namespace Benchmark.Reflection
 
         public class Data1
         {
-            public string P1 { get; }
+            public string? P1 { get; }
 
-            public Data1(string p1)
+            public Data1(string? p1)
             {
                 P1 = p1;
             }
@@ -172,16 +127,16 @@ namespace Benchmark.Reflection
 
         public class Data8
         {
-            public string P1 { get; }
-            public string P2 { get; }
-            public string P3 { get; }
-            public string P4 { get; }
-            public string P5 { get; }
-            public string P6 { get; }
-            public string P7 { get; }
-            public string P8 { get; }
+            public string? P1 { get; }
+            public string? P2 { get; }
+            public string? P3 { get; }
+            public string? P4 { get; }
+            public string? P5 { get; }
+            public string? P6 { get; }
+            public string? P7 { get; }
+            public string? P8 { get; }
 
-            public Data8(string p1, string p2, string p3, string p4, string p5, string p6, string p7, string p8)
+            public Data8(string? p1, string? p2, string? p3, string? p4, string? p5, string? p6, string? p7, string? p8)
             {
                 P1 = p1;
                 P2 = p2;
--- Benchmark/Reflection/DelegateFactoryBenchmark.cs
+++ Benchmark/Reflection/DelegateFactoryBenchmark.cs
@@ -15,72 +15,72 @@ namespace Benchmark.Reflection
         private static readonly ConstructorInfo Ci1 = typeof(Data1).GetConstructors()[0];
         private static readonly ConstructorInfo Ci2 = typeof(Data2).GetConstructors()[0];
 
-        private static readonly PropertyInfo Pii = typeof(MemberData).GetProperty(nameof(MemberData.IntValue));
-        private static readonly PropertyInfo Pis = typeof(MemberData).GetProperty(nameof(MemberData.StringValue));
-        private static readonly PropertyInfo PiHi = typeof(MemberData).GetProperty(nameof(MemberData.NotificationIntValue));
-        private static readonly PropertyInfo PiHs = typeof(MemberData).GetProperty(nameof(MemberData.NotificationStringValue));
+        private static readonly PropertyInfo Pii = typeof(MemberData).GetProperty(nameof(MemberData.IntValue))!;
+        private static readonly PropertyInfo Pis = typeof(MemberData).GetProperty(nameof(MemberData.StringValue))!;
+        private static readonly PropertyInfo PiHi = typeof(MemberData).GetProperty(nameof(MemberData.NotificationIntValue))!;
+        private static readonly PropertyInfo PiHs = typeof(MemberData).GetProperty(nameof(MemberData.NotificationStringValue))!;
 
         // Dynamic
-        private Func<object> factory0Dynamic;
-        private Func<object, object> factory1Dynamic;
-        private Func<object, object, object> factory2Dynamic;
-        private Func<object[], object> factory2BDynamic;
+        private Func<object>? factory0Dynamic;
+        private Func<object?, object>? factory1Dynamic;
+        private Func<object?, object?, object>? factory2Dynamic;
+        private Func<object?[]?, object>? factory2BDynamic;
 
-        private Func<Data0> typedFactory0Dynamic;
-        private Func<string, Data1> typedFactory1Dynamic;
-        private Func<string, int, Data2> typedFactory2Dynamic;
+        private Func<Data0>? typedFactory0Dynamic;
+        private Func<string?, Data1>? typedFactory1Dynamic;
+        private Func<string?, int, Data2>? typedFactory2Dynamic;
 
-        private Func<object, object> getterIntDynamic;
-        private Func<object, object> getterStringDynamic;
-        private Func<object, object> getterHolderIntDynamic;
-        private Func<object, object> getterHolderStringDynamic;
+        private Func<object, object?>? getterIntDynamic;
+        private Func<object, object?>? getterStringDynamic;
+        private Func<object, object?>? getterHolderIntDynamic;
+        private Func<object, object?>? getterHolderStringDynamic;
 
-        private Func<MemberData, int> typedGetterIntDynamic;
-        private Func<MemberData, string> typedGetterStringDynamic;
-        private Func<MemberData, int> typedGetterHolderIntDynamic;
-        private Func<MemberData, string> typedGetterHolderStringDynamic;
+        private Func<MemberData, int>? typedGetterIntDynamic;
+        private Func<MemberData, string?>? typedGetterStringDynamic;
+        private Func<MemberData, int>? typedGetterHolderIntDynamic;
+        private Func<MemberData, string?>? typedGetterHolderStringDynamic;
 
-        private Action<object, object> setterIntDynamic;
-        private Action<object, object> setterStringDynamic;
-        private Action<object, object> setterHolderIntDynamic;
-        private Action<object, object> setterHolderStringDynamic;
+        private Action<object, object?>? setterIntDynamic;
+        private Action<object, object?>? setterStringDynamic;
+        private Action<object, object?>? setterHolderIntDynamic;
+        private Action<object, object?>? setterHolderStringDynamic;
 
-        private Action<MemberData, int> typedSetterIntDynamic;
-        private Action<MemberData, string> typedSetterStringDynamic;
-        private Action<MemberData, int> typedSetterHolderIntDynamic;
-        private Action<MemberData, string> typedSetterHolderStringDynamic;
+        private Action<MemberData, int>? typedSetterIntDynamic;
+        private Action<MemberData, string?>? typedSetterStringDynamic;
+        private Action<MemberData, int>? typedSetterHolderIntDynamic;
+        private Action<MemberData, string?>? typedSetterHolderStringDynamic;
 
         // Reflection
-        private Func<object> factory0Reflection;
-        private Func<object, object> factory1Reflection;
-        private Func<object, object, object> factory2Reflection;
-        private Func<object[], object> factory2BReflection;
+        private Func<object>? factory0Reflection;
+        private Func<object?, object>? factory1Reflection;
+        private Func<object?, object?, object>? factory2Reflection;
+        private Func<object?[]?, object>? factory2BReflection;
 
-        private Func<Data0> typedFactory0Reflection;
-        private Func<string, Data1> typedFactory1Reflection;
-        private Func<string, int, Data2> typedFactory2Reflection;
+        private Func<Data0>? typedFactory0Reflection;
+        private Func<string?, Data1>? typedFactory1Reflection;
+        private Func<string?, int, Data2>? typedFactory2Reflection;
 
-        private Func<object, object> getterIntReflection;
-        private Func<object, object> getterStringReflection;
-        private Func<object, object> getterHolderIntReflection;
-        private Func<object, object> getterHolderStringReflection;
+        private Func<object, object?>? getterIntReflection;
+        private Func<object, object?>? getterStringReflection;
+        private Func<object, object?>? getterHolderIntReflection;
+        private Func<object, object?>? getterHolderStringReflection;
 
-        private Func<MemberData, int> typedGetterIntReflection;
-        private Func<MemberData, string> typedGetterStringReflection;
-        private Func<MemberData, int> typedGetterHolderIntReflection;
-        private Func<MemberData, string> typedGetterHolderStringReflection;
+        private Func<MemberData, int>? typedGetterIntReflection;
+        private Func<MemberData, string?>? typedGetterStringReflection;
+        private Func<MemberData, int>? typedGetterHolderIntReflection;
+        private Func<MemberData, string?>? typedGetterHolderStringReflection;
 
-        private Action<object, object> setterIntReflection;
-        private Action<object, object> setterStringReflection;
-        private Action<object, object> setterHolderIntReflection;
-        private Action<object, object> setterHolderStringReflection;
+        private Action<object, object?>? setterIntReflection;
+        private Action<object, object?>? setterStringReflection;
+        private Action<object, object?>? setterHolderIntReflection;
+        private Action<object, object?>? setterHolderStringReflection;
 
-        private Action<MemberData, int> typedSetterIntReflection;
-        private Action<MemberData, string> typedSetterStringReflection;
-        private Action<MemberData, int> typedSetterHolderIntReflection;
-        private Action<MemberData, string> typedSetterHolderStringReflection;
+        private Action<MemberData, int>? typedSetterIntReflection;
+        private Action<MemberData, string?>? typedSetterStringReflection;
+        private Action<MemberData, int>? typedSetterHolderIntReflection;
+        private Action<MemberData, string?>? typedSetterHolderStringReflection;
 
-        private MemberData data;
+        private MemberData? data;
 
         [GlobalSetup]
         public void Setup()
@@ -95,10 +95,10 @@ namespace Benchmark.Reflection
             typedFactory1Dynamic = DynamicDelegateFactory.Default.CreateFactory<string, Data1>();
             typedFactory2Dynamic = DynamicDelegateFactory.Default.CreateFactory<string, int, Data2>();
 
-            getterIntDynamic = DynamicDelegateFactory.Default.CreateGetter(Pii);
-            getterStringDynamic = DynamicDelegateFactory.Default.CreateGetter(Pis);
-            getterHolderIntDynamic = DynamicDelegateFactory.Default.CreateGetter(PiHi);
-            getterHolderStringDynamic = DynamicDelegateFactory.Default.CreateGetter(PiHs);
+            getterIntDynamic = DynamicDelegateFactory.Default.CreateGetter(Pii)!;
+            getterStringDynamic = DynamicDelegateFactory.Default.CreateGetter(Pis)!;
+            getterHolderIntDynamic = DynamicDelegateFactory.Default.CreateGetter(PiHi)!;
+            getterHolderStringDynamic = DynamicDelegateFactory.Default.CreateGetter(PiHs)!;
 
             typedGetterIntDynamic = DynamicDelegateFactory.Default.CreateGetter<MemberData, int>(Pii);
             typedGetterStringDynamic = DynamicDelegateFactory.Default.CreateGetter<MemberData, string>(Pis);
@@ -155,130 +155,130 @@ namespace Benchmark.Reflection
         // Factory
 
         [Benchmark]
-        public object Factory0Dynamic() => factory0Dynamic();
+        public object Factory0Dynamic() => factory0Dynamic!();
         [Benchmark]
-        public object Factory1Dynamic() => factory1Dynamic(string.Empty);
+        public object Factory1Dynamic() => factory1Dynamic!(string.Empty);
         [Benchmark]
-        public object Factory2Dynamic() => factory2Dynamic(string.Empty, 0);
+        public object Factory2Dynamic() => factory2Dynamic!(string.Empty, 0);
         [Benchmark]
-        public object Factory2BDynamic() => factory2BDynamic(new object[] { string.Empty, 0 });
+        public object Factory2BDynamic() => factory2BDynamic!(new object[] { string.Empty, 0 });
 
         // TypedFactory
 
         [Benchmark]
-        public Data0 TypedFactory0Dynamic() => typedFactory0Dynamic();
+        public Data0 TypedFactory0Dynamic() => typedFactory0Dynamic!();
         [Benchmark]
-        public Data1 TypedFactory1Dynamic() => typedFactory1Dynamic(string.Empty);
+        public Data1 TypedFactory1Dynamic() => typedFactory1Dynamic!(string.Empty);
         [Benchmark]
-        public Data2 TypedFactory2Dynamic() => typedFactory2Dynamic(string.Empty, 0);
+        public Data2 TypedFactory2Dynamic() => typedFactory2Dynamic!(string.Empty, 0);
 
         // Getter
 
         [Benchmark]
-        public object GetterIntDynamic() => getterIntDynamic(data);
+        public object? GetterIntDynamic() => getterIntDynamic!(data!);
         [Benchmark]
-        public object GetterStringDynamic() => getterStringDynamic(data);
+        public object? GetterStringDynamic() => getterStringDynamic!(data!);
         [Benchmark]
-        public object GetterHolderIntDynamic() => getterHolderIntDynamic(data);
+        public object? GetterHolderIntDynamic() => getterHolderIntDynamic!(data!);
         [Benchmark]
-        public object GetterHolderStringDynamic() => getterHolderStringDynamic(data);
+        public object? GetterHolderStringDynamic() => getterHolderStringDynamic!(data!);
 
         // TypedGetter
 
         [Benchmark]
-        public int TypedGetterIntDynamic() => typedGetterIntDynamic(data);
+        public int TypedGetterIntDynamic() => typedGetterIntDynamic!(data!);
         [Benchmark]
-        public string TypedGetterStringDynamic() => typedGetterStringDynamic(data);
+        public string? TypedGetterStringDynamic() => typedGetterStringDynamic!(data!);
         [Benchmark]
-        public int TypedGetterHolderIntDynamic() => typedGetterHolderIntDynamic(data);
+        public int TypedGetterHolderIntDynamic() => typedGetterHolderIntDynamic!(data!);
         [Benchmark]
-        public string TypedGetterHolderStringDynamic() => typedGetterHolderStringDynamic(data);
+        public string? TypedGetterHolderStringDynamic() => typedGetterHolderStringDynamic!(data!);
 
         // Setter
 
         [Benchmark]
-        public void SetterIntDynamic() => setterIntDynamic(data, 0);
+        public void SetterIntDynamic() => setterIntDynamic!(data!, 0);
         [Benchmark]
-        public void SetterStringDynamic() => setterStringDynamic(data, string.Empty);
+        public void SetterStringDynamic() => setterStringDynamic!(data!, string.Empty);
         [Benchmark]
-        public void SetterHolderIntDynamic() => setterHolderIntDynamic(data, 0);
+        public void SetterHolderIntDynamic() => setterHolderIntDynamic!(data!, 0);
         [Benchmark]
-        public void SetterHolderStringDynamic() => setterHolderStringDynamic(data, string.Empty);
+        public void SetterHolderStringDynamic() => setterHolderStringDynamic!(data!, string.Empty);
 
         // TypedSetter
 
         [Benchmark]
-        public void TypedSetterIntDynamic() => typedSetterIntDynamic(data, 0);
+        public void TypedSetterIntDynamic() => typedSetterIntDynamic!(data!, 0);
         [Benchmark]
-        public void TypedSetterStringDynamic() => typedSetterStringDynamic(data, string.Empty);
+        public void TypedSetterStringDynamic() => typedSetterStringDynamic!(data!, string.Empty);
         [Benchmark]
-        public void TypedSetterHolderIntDynamic() => typedSetterHolderIntDynamic(data, 0);
+        public void TypedSetterHolderIntDynamic() => typedSetterHolderIntDynamic!(data!, 0);
         [Benchmark]
-        public void TypedSetterHolderStringDynamic() => typedSetterHolderStringDynamic(data, string.Empty);
+        public void TypedSetterHolderStringDynamic() => typedSetterHolderStringDynamic!(data!, string.Empty);
 
         // Factory
 
         [Benchmark]
-        public object Factory0Reflection() => factory0Reflection();
+        public object Factory0Reflection() => factory0Reflection!();
         [Benchmark]
-        public object Factory1Reflection() => factory1Reflection(string.Empty);
+        public object Factory1Reflection() => factory1Reflection!(string.Empty);
         [Benchmark]
-        public object Factory2Reflection() => factory2Reflection(string.Empty, 0);
+        public object Factory2Reflection() => factory2Reflection!(string.Empty, 0);
         [Benchmark]
-        public object Factory2BReflection() => factory2BReflection(new object[] { string.Empty, 0 });
+        public object Factory2BReflection() => factory2BReflection!(new object[] { string.Empty, 0 });
 
         // TypedFactory
 
         [Benchmark]
-        public Data0 TypedFactory0Reflection() => typedFactory0Reflection();
+        public Data0 TypedFactory0Reflection() => typedFactory0Reflection!();
         [Benchmark]
-        public Data1 TypedFactory1Reflection() => typedFactory1Reflection(string.Empty);
+        public Data1 TypedFactory1Reflection() => typedFactory1Reflection!(string.Empty);
         [Benchmark]
-        public Data2 TypedFactory2Reflection() => typedFactory2Reflection(string.Empty, 0);
+        public Data2 TypedFactory2Reflection() => typedFactory2Reflection!(string.Empty, 0);
 
         // Getter
 
         [Benchmark]
-        public object GetterIntReflection() => getterIntReflection(data);
+        public object? GetterIntReflection() => getterIntReflection!(data!);
         [Benchmark]
-        public object GetterStringReflection() => getterStringReflection(data);
+        public object? GetterStringReflection() => getterStringReflection!(data!);
         [Benchmark]
-        public object GetterHolderIntReflection() => getterHolderIntReflection(data);
+        public object? GetterHolderIntReflection() => getterHolderIntReflection!(data!);
         [Benchmark]
-        public object GetterHolderStringReflection() => getterHolderStringReflection(data);
+        public object? GetterHolderStringReflection() => getterHolderStringReflection!(data!);
 
         // TypedGetter
 
         [Benchmark]
-        public int TypedGetterIntReflection() => typedGetterIntReflection(data);
+        public int TypedGetterIntReflection() => typedGetterIntReflection!(data!);
         [Benchmark]
-        public string TypedGetterStringReflection() => typedGetterStringReflection(data);
+        public string? TypedGetterStringReflection() => typedGetterStringReflection!(data!);
         [Benchmark]
-        public int TypedGetterHolderIntReflection() => typedGetterHolderIntReflection(data);
+        public int TypedGetterHolderIntReflection() => typedGetterHolderIntReflection!(data!);
         [Benchmark]
-        public string TypedGetterHolderStringReflection() => typedGetterHolderStringReflection(data);
+        public string? TypedGetterHolderStringReflection() => typedGetterHolderStringReflection!(data!);
 
         // Setter
 
         [Benchmark]
-        public void SetterIntReflection() => setterIntReflection(data, 0);
+        public void SetterIntReflection() => setterIntReflection!(data!, 0);
         [Benchmark]
-        public void SetterStringReflection() => setterStringReflection(data, string.Empty);
+        public void SetterStringReflection() => setterStringReflection!(data!, string.Empty);
         [Benchmark]
-        public void SetterHolderIntReflection() => setterHolderIntReflection(data, 0);
+        public void SetterHolderIntReflection() => setterHolderIntReflection!(data!, 0);
         [Benchmark]
-        public void SetterHolderStringReflection() => setterHolderStringReflection(data, string.Empty);
+        public void SetterHolderStringReflection() => setterHolderStringReflection!(data!, string.Empty);
 
         // TypedSetter
 
         [Benchmark]
-        public void TypedSetterIntReflection() => typedSetterIntReflection(data, 0);
+        public void TypedSetterIntReflection() => typedSetterIntReflection!(data!, 0);
         [Benchmark]
-        public void TypedSetterStringReflection() => typedSetterStringReflection(data, string.Empty);
+        public void TypedSetterStringReflection() => typedSetterStringReflection!(data!, string.Empty);
         [Benchmark]
-        public void TypedSetterHolderIntReflection() => typedSetterHolderIntReflection(data, 0);
+        public void TypedSetterHolderIntReflection() => typedSetterHolderIntReflection!(data!, 0);
         [Benchmark]
-        public void TypedSetterHolderStringReflection() => typedSetterHolderStringReflection(data, string.Empty);
+        public void TypedSetterHolderStringReflection() => typedSetterHolderStringReflection!(data!, string.Empty);
 
         // Data
 
@@ -286,9 +286,9 @@ namespace Benchmark.Reflection
         {
             public int IntValue { get; set; }
 
-            public string StringValue { get; set; }
+            public string? StringValue { get; set; }
 
-            public IValueHolder<string> NotificationStringValue { get; } = new NotificationValue<string>();
+            public IValueHolder<string?> NotificationStringValue { get; } = new NotificationValue<string?>();
 
             public IValueHolder<int> NotificationIntValue { get; } = new NotificationValue<int>();
         }
@@ -299,9 +299,9 @@ namespace Benchmark.Reflection
 
         public class Data1
         {
-            public string P1 { get; }
+            public string? P1 { get; }
 
-            public Data1(string p1)
+            public Data1(string? p1)
             {
                 P1 = p1;
             }
@@ -309,11 +309,11 @@ namespace Benchmark.Reflection
 
         public class Data2
         {
-            public string P1 { get; }
+            public string? P1 { get; }
 
             public int P2 { get; }
 
-            public Data2(string p1, int p2)
+            public Data2(string? p1, int p2)
             {
                 P1 = p1;
                 P2 = p2;
