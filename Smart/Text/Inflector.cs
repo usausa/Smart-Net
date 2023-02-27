34ce38b8966cfbc9c6b9e0282be7deb6dc34df66
usausa <machi.pon@gmail.com>
2023-02-27 11:54:38 +0900

    Update inflector

--- Smart/Text/Inflector.cs
+++ Smart/Text/Inflector.cs
@@ -1,6 +1,7 @@
 namespace Smart.Text;
 
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 public static class Inflector
 {
@@ -17,40 +18,48 @@ public static class Inflector
             return string.Empty;
         }
 
-        var buffer = word.Length < 512 ? stackalloc char[word.Length] : new char[word.Length];
-        var length = 0;
+        var length = word.Length;
+        var buffer = length < 512 ? stackalloc char[length] : new char[length];
 
-        fixed (char* pBuffer = buffer)
+        ref var start = ref MemoryMarshal.GetReference(buffer);
+        ref var b = ref start;
+
+        ref var c = ref MemoryMarshal.GetReference(word);
+        ref var end = ref Unsafe.Add(ref c, length);
+
+        var isLowerPrevious = false;
+        do
         {
-            var isLowerPrevious = false;
-            foreach (var c in word)
+            if (c == '_')
             {
-                if (c == '_')
+                toUpper = true;
+            }
+            else
+            {
+                if (toUpper)
                 {
-                    toUpper = true;
+                    b = Char.ToUpperInvariant(c);
+                    b = ref Unsafe.Add(ref b, 1);
+                    toUpper = false;
+                }
+                else if (isLowerPrevious)
+                {
+                    b = c;
+                    b = ref Unsafe.Add(ref b, 1);
                 }
                 else
                 {
-                    if (toUpper)
-                    {
-                        pBuffer[length++] = Char.ToUpperInvariant(c);
-                        toUpper = false;
-                    }
-                    else if (isLowerPrevious)
-                    {
-                        pBuffer[length++] = c;
-                    }
-                    else
-                    {
-                        pBuffer[length++] = Char.ToLowerInvariant(c);
-                    }
-
-                    isLowerPrevious = Char.IsLower(c);
+                    b = Char.ToLowerInvariant(c);
+                    b = ref Unsafe.Add(ref b, 1);
                 }
+
+                isLowerPrevious = Char.IsLower(c);
             }
 
-            return new string(pBuffer, 0, length);
-        }
+            c = ref Unsafe.Add(ref c, 1);
+        } while (Unsafe.IsAddressLessThan(ref c, ref end));
+
+        return new string(buffer[..((int)Unsafe.ByteOffset(ref start, ref b) >> 1)]);
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -63,52 +72,83 @@ public static class Inflector
             return string.Empty;
         }
 
-        var bufferSize = word.Length << 1;
+        var length = word.Length;
+        var bufferSize = length << 1;
         var buffer = bufferSize < 512 ? stackalloc char[bufferSize] : new char[bufferSize];
-        var length = 0;
 
-        fixed (char* pBuffer = buffer)
+        ref var start = ref MemoryMarshal.GetReference(buffer);
+        ref var b = ref start;
+
+        ref var c = ref MemoryMarshal.GetReference(word);
+        ref var end = ref Unsafe.Add(ref c, length);
+
+        if (toUpper)
         {
-            if (toUpper)
+            if (Char.IsUpper(c))
             {
-                foreach (var c in word)
-                {
-                    if (Char.IsUpper(c))
-                    {
-                        if (length > 0)
-                        {
-                            pBuffer[length++] = '_';
-                        }
-
-                        pBuffer[length++] = c;
-                    }
-                    else
-                    {
-                        pBuffer[length++] = Char.ToUpperInvariant(c);
-                    }
-                }
+                b = c;
+                b = ref Unsafe.Add(ref b, 1);
             }
             else
             {
-                foreach (var c in word)
-                {
-                    if (Char.IsUpper(c))
-                    {
-                        if (length > 0)
-                        {
-                            pBuffer[length++] = '_';
-                        }
-
-                        pBuffer[length++] = Char.ToLowerInvariant(c);
-                    }
-                    else
-                    {
-                        pBuffer[length++] = c;
-                    }
-                }
+                b = Char.ToUpperInvariant(c);
+                b = ref Unsafe.Add(ref b, 1);
             }
 
-            return new string(pBuffer, 0, length);
+            c = ref Unsafe.Add(ref c, 1);
+
+            while (Unsafe.IsAddressLessThan(ref c, ref end))
+            {
+                if (Char.IsUpper(c))
+                {
+                    b = '_';
+                    b = ref Unsafe.Add(ref b, 1);
+                    b = c;
+                    b = ref Unsafe.Add(ref b, 1);
+                }
+                else
+                {
+                    b = Char.ToUpperInvariant(c);
+                    b = ref Unsafe.Add(ref b, 1);
+                }
+
+                c = ref Unsafe.Add(ref c, 1);
+            }
         }
+        else
+        {
+            if (Char.IsUpper(c))
+            {
+                b = Char.ToLowerInvariant(c);
+                b = ref Unsafe.Add(ref b, 1);
+            }
+            else
+            {
+                b = c;
+                b = ref Unsafe.Add(ref b, 1);
+            }
+
+            c = ref Unsafe.Add(ref c, 1);
+
+            while (Unsafe.IsAddressLessThan(ref c, ref end))
+            {
+                if (Char.IsUpper(c))
+                {
+                    b = '_';
+                    b = ref Unsafe.Add(ref b, 1);
+                    b = Char.ToLowerInvariant(c);
+                    b = ref Unsafe.Add(ref b, 1);
+                }
+                else
+                {
+                    b = c;
+                    b = ref Unsafe.Add(ref b, 1);
+                }
+
+                c = ref Unsafe.Add(ref c, 1);
+            }
+        }
+
+        return new string(buffer[..((int)Unsafe.ByteOffset(ref start, ref b) >> 1)]);
     }
 }
