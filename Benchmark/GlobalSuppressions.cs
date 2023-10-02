4a4c4d01491664a8a60fcc5c9d86e1e6cad733c3
usausa <machi.pon@gmail.com>
2023-10-02 11:53:39 +0900

    Update setting

--- Benchmark/GlobalSuppressions.cs
+++ Benchmark/GlobalSuppressions.cs
@@ -1,1 +1,1 @@
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "Ignore")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:Mark members as static", Justification = "Ignore")]
---
+++ Smart.Tests/GlobalSuppressions.cs
@@ -0,0 +1,1 @@
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1861:Avoid constant arrays as arguments\r\n", Justification = "Ignore")]
