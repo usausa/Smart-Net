77eb6962266a914c3ed1d65c18a137424f772651
usausa <machi.pon@gmail.com>
2025-06-21 19:43:15 +0900

    Update extensions

--- Smart/Linq/EnumerableExtensions.WhereNotNull.cs
+++ Smart/Linq/EnumerableExtensions.WhereNotNull.cs
@@ -5,5 +5,12 @@ using System.Runtime.CompilerServices;
 public static partial class EnumerableExtensions
 {
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> source) => source.Where(static x => x is not null)!;
+    public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> source)
+        where T : class =>
+        source.Where(static x => x is not null)!;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> source)
+        where T : struct =>
+        source.Where(static x => x.HasValue).Select(static x => x!.Value);
 }
