e397ea7bd9a2901e7204cded71359997f281e729
usausa <machi.pon@gmail.com>
2026-07-20 09:47:53 +0900

    Update

--- Smart/Enums.cs
+++ Smart/Enums.cs
@@ -2,14 +2,17 @@ namespace Smart;
 
 using System.Collections.ObjectModel;
 
+#pragma warning disable CA1000
 public static class Enums<T>
     where T : struct, Enum
 {
-    private static readonly Type EnumType = typeof(T);
-
     private static readonly Dictionary<T, string> ValueToNames;
 
     private static readonly Dictionary<string, T> NameToValues;
+
+#if NET9_0_OR_GREATER
+    private static readonly Dictionary<string, T>.AlternateLookup<ReadOnlySpan<char>> NameToValuesLookup;
+#endif
 
     public static ReadOnlyCollection<T> Values { get; }
 
@@ -37,6 +40,10 @@ public static class Enums<T>
             ValueToNames[values[i]] = names[i];
             NameToValues[names[i]] = values[i];
         }
+
+#if NET9_0_OR_GREATER
+        NameToValuesLookup = NameToValues.GetAlternateLookup<ReadOnlySpan<char>>();
+#endif
     }
 
     public static string GetName(T value)
@@ -46,11 +53,30 @@ public static class Enums<T>
 
     public static T ParseValue(string name)
     {
-        return NameToValues.TryGetValue(name, out var value) ? value : (T)Enum.Parse(EnumType, name);
+        return NameToValues.TryGetValue(name, out var value) ? value : Enum.Parse<T>(name);
     }
 
     public static bool TryParseValue(string name, out T value)
     {
         return NameToValues.TryGetValue(name, out value) || Enum.TryParse(name, out value);
     }
+
+    public static T ParseValue(ReadOnlySpan<char> name)
+    {
+#if NET9_0_OR_GREATER
+        return NameToValuesLookup.TryGetValue(name, out var value) ? value : Enum.Parse<T>(name);
+#else
+        return Enum.Parse<T>(name);
+#endif
+    }
+
+    public static bool TryParseValue(ReadOnlySpan<char> name, out T value)
+    {
+#if NET9_0_OR_GREATER
+        return NameToValuesLookup.TryGetValue(name, out value) || Enum.TryParse(name, out value);
+#else
+        return Enum.TryParse(name, out value);
+#endif
+    }
 }
+#pragma warning disable CA1000
