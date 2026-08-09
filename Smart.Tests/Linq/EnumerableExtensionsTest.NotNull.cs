93bf7e6bbfd9460f42a4fc8bfdbc40b9e63bba2b
usausa <machi.pon@gmail.com>
2026-08-09 15:16:58 +0900

    Update

---
+++ Smart.Tests/Linq/EnumerableExtensionsTest.NotNull.cs
@@ -0,0 +1,215 @@
+namespace Smart.Linq;
+
+public sealed class EnumerableExtensionsNotNullTest
+{
+    private static IEnumerable<T> Lazy<T>(IEnumerable<T> source)
+    {
+        foreach (var value in source)
+        {
+            yield return value;
+        }
+    }
+
+    //--------------------------------------------------------------------------------
+    // WhereNotNull : struct
+    //--------------------------------------------------------------------------------
+
+    [Fact]
+    public void WhereNotNullStructFromArray()
+    {
+        int?[] source = [1, null, 2, null, 3];
+
+        Assert.Equal([1, 2, 3], source.WhereNotNull());
+    }
+
+    [Fact]
+    public void WhereNotNullStructFromList()
+    {
+        var source = new List<int?> { 1, null, 2, null, 3 };
+
+        Assert.Equal([1, 2, 3], source.WhereNotNull());
+    }
+
+    [Fact]
+    public void WhereNotNullStructFromEnumerable()
+    {
+        var source = Lazy<int?>([1, null, 2, null, 3]);
+
+        Assert.Equal([1, 2, 3], source.WhereNotNull());
+    }
+
+    [Fact]
+    public void WhereNotNullStructAllNull()
+    {
+        int?[] array = [null, null];
+        var list = new List<int?> { null, null };
+
+        Assert.Empty(array.WhereNotNull());
+        Assert.Empty(list.WhereNotNull());
+        Assert.Empty(Lazy<int?>([null, null]).WhereNotNull());
+    }
+
+    [Fact]
+    public void WhereNotNullStructEmpty()
+    {
+        Assert.Empty(Array.Empty<int?>().WhereNotNull());
+        Assert.Empty(new List<int?>().WhereNotNull());
+    }
+
+    [Fact]
+    public void WhereNotNullStructDefaultValueIsPreserved()
+    {
+        // default(int) は null ではないので保持される
+        int?[] array = [0, null, 0];
+        var list = new List<int?> { 0, null, 0 };
+
+        Assert.Equal([0, 0], array.WhereNotNull());
+        Assert.Equal([0, 0], list.WhereNotNull());
+    }
+
+    //--------------------------------------------------------------------------------
+    // WhereNotNull : class
+    //--------------------------------------------------------------------------------
+
+    [Fact]
+    public void WhereNotNullClass()
+    {
+        string?[] source = ["a", null, "b"];
+
+        Assert.Equal(["a", "b"], source.WhereNotNull());
+    }
+
+    //--------------------------------------------------------------------------------
+    // SelectNotNull : struct
+    //--------------------------------------------------------------------------------
+
+    [Fact]
+    public void SelectNotNullStructFromArray()
+    {
+        int[] source = [1, 2, 3, 4];
+
+        Assert.Equal([2, 4], source.SelectNotNull(static x => (x % 2) == 0 ? (int?)x : null));
+    }
+
+    [Fact]
+    public void SelectNotNullStructFromList()
+    {
+        var source = new List<int> { 1, 2, 3, 4 };
+
+        Assert.Equal([2, 4], source.SelectNotNull(static x => (x % 2) == 0 ? (int?)x : null));
+    }
+
+    [Fact]
+    public void SelectNotNullStructFromEnumerable()
+    {
+        var source = Lazy([1, 2, 3, 4]);
+
+        Assert.Equal([2, 4], source.SelectNotNull(static x => (x % 2) == 0 ? (int?)x : null));
+    }
+
+    //--------------------------------------------------------------------------------
+    // SelectNotNull : class
+    //--------------------------------------------------------------------------------
+
+    [Fact]
+    public void SelectNotNullClass()
+    {
+        int[] source = [1, 2, 3, 4];
+
+        Assert.Equal(["2", "4"], source.SelectNotNull(static x => (x % 2) == 0 ? x.ToString(System.Globalization.CultureInfo.InvariantCulture) : null));
+    }
+
+    //--------------------------------------------------------------------------------
+    // イテレータの契約
+    //--------------------------------------------------------------------------------
+
+    private static List<T> Materialize<T>(IEnumerable<T> source)
+    {
+        var result = new List<T>();
+        using var enumerator = source.GetEnumerator();
+        while (enumerator.MoveNext())
+        {
+            result.Add(enumerator.Current);
+        }
+
+        return result;
+    }
+
+    // ReSharper disable PossibleMultipleEnumeration
+    [Fact]
+    public void EnumerableCanBeEnumeratedTwice()
+    {
+        int?[] array = [1, null, 2];
+        var list = new List<int?> { 1, null, 2 };
+
+        var fromArray = array.WhereNotNull();
+        Assert.Equal([1, 2], Materialize(fromArray));
+        Assert.Equal([1, 2], Materialize(fromArray));
+
+        var fromList = list.WhereNotNull();
+        Assert.Equal([1, 2], Materialize(fromList));
+        Assert.Equal([1, 2], Materialize(fromList));
+    }
+    // ReSharper restore PossibleMultipleEnumeration
+
+    // ReSharper disable PossibleMultipleEnumeration
+    [Fact]
+    public void NestedEnumerationIsIndependent()
+    {
+        var list = new List<int?> { 1, 2 };
+        var source = list.WhereNotNull();
+
+        var pairs = new List<(int, int)>();
+        foreach (var x in source)
+        {
+            foreach (var y in source)
+            {
+                pairs.Add((x, y));
+            }
+        }
+
+        Assert.Equal([(1, 1), (1, 2), (2, 1), (2, 2)], pairs);
+    }
+    // ReSharper restore PossibleMultipleEnumeration
+
+    [Fact]
+    public void ExecutionIsDeferred()
+    {
+        var called = false;
+
+        var source = new List<int> { 1 }.SelectNotNull(int? (x) =>
+        {
+            called = true;
+            return x;
+        });
+
+        Assert.False(called);
+
+        _ = source.ToList();
+
+        Assert.True(called);
+    }
+
+    [Fact]
+    public void ListModificationDuringEnumerationThrows()
+    {
+        var list = new List<int?> { 1, 2, 3 };
+
+        Assert.Throws<InvalidOperationException>(() =>
+        {
+            foreach (var value in list.WhereNotNull())
+            {
+                list.Add(value);
+            }
+        });
+    }
+
+    [Fact]
+    public void ResetIsNotSupported()
+    {
+        var list = new List<int?> { 1 };
+        using var enumerator = list.WhereNotNull().GetEnumerator();
+
+        Assert.Throws<NotSupportedException>(enumerator.Reset);
+    }
+}
--- Smart/Linq/EnumerableExtensions.NotNull.cs
+++ Smart/Linq/EnumerableExtensions.NotNull.cs
@@ -1,5 +1,6 @@
 namespace Smart.Linq;
 
+using System.Collections;
 using System.Linq;
 using System.Runtime.CompilerServices;
 
@@ -10,7 +11,24 @@ public static partial class EnumerableExtensions
         where TResult : class =>
         (IEnumerable<TResult>)source.Select(selector).Where(static x => x is not null);
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static IEnumerable<TResult> SelectNotNull<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult?> selector)
+        where TResult : struct
+    {
+        if (source is List<TSource> list)
+        {
+            return new SelectNotNullListIterator<TSource, TResult>(list, selector);
+        }
+
+        if (source is TSource[] array)
+        {
+            return new SelectNotNullArrayIterator<TSource, TResult>(array, selector);
+        }
+
+        return SelectNotNullIterator(source, selector);
+    }
+
+    private static IEnumerable<TResult> SelectNotNullIterator<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult?> selector)
         where TResult : struct
     {
         foreach (var item in source)
@@ -28,7 +46,24 @@ public static partial class EnumerableExtensions
         where TResult : class =>
         (IEnumerable<TResult>)source.Where(static x => x is not null);
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static IEnumerable<TResult> WhereNotNull<TResult>(this IEnumerable<TResult?> source)
+        where TResult : struct
+    {
+        if (source is List<TResult?> list)
+        {
+            return new WhereNotNullListIterator<TResult>(list);
+        }
+
+        if (source is TResult?[] array)
+        {
+            return new WhereNotNullArrayIterator<TResult>(array);
+        }
+
+        return WhereNotNullIterator(source);
+    }
+
+    private static IEnumerable<TResult> WhereNotNullIterator<TResult>(IEnumerable<TResult?> source)
         where TResult : struct
     {
         foreach (var value in source)
@@ -37,6 +72,244 @@ public static partial class EnumerableExtensions
             {
                 yield return value.GetValueOrDefault();
             }
+        }
+    }
+
+    //--------------------------------------------------------------------------------
+    // Iterator
+    //--------------------------------------------------------------------------------
+
+    private sealed class SelectNotNullListIterator<TSource, TResult> : IEnumerable<TResult>, IEnumerator<TResult>
+        where TResult : struct
+    {
+        private readonly List<TSource> source;
+
+        private readonly Func<TSource, TResult?> selector;
+
+        private readonly int threadId;
+
+        private int state;
+
+        private List<TSource>.Enumerator enumerator;
+
+        public SelectNotNullListIterator(List<TSource> source, Func<TSource, TResult?> selector)
+        {
+            this.source = source;
+            this.selector = selector;
+            threadId = Environment.CurrentManagedThreadId;
+        }
+
+        public TResult Current { get; private set; }
+
+        object IEnumerator.Current => Current;
+
+        public IEnumerator<TResult> GetEnumerator()
+        {
+            if ((state == 0) && (threadId == Environment.CurrentManagedThreadId))
+            {
+                state = 1;
+                enumerator = source.GetEnumerator();
+                return this;
+            }
+
+            return new SelectNotNullListIterator<TSource, TResult>(source, selector) { state = 1, enumerator = source.GetEnumerator() };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public bool MoveNext()
+        {
+            while (enumerator.MoveNext())
+            {
+                var value = selector(enumerator.Current);
+                if (value.HasValue)
+                {
+                    Current = value.GetValueOrDefault();
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void Reset() => throw new NotSupportedException();
+
+        public void Dispose() => enumerator.Dispose();
+    }
+
+    private sealed class SelectNotNullArrayIterator<TSource, TResult> : IEnumerable<TResult>, IEnumerator<TResult>
+        where TResult : struct
+    {
+        private readonly TSource[] source;
+
+        private readonly Func<TSource, TResult?> selector;
+
+        private readonly int threadId;
+
+        private int state;
+
+        private int index;
+
+        public SelectNotNullArrayIterator(TSource[] source, Func<TSource, TResult?> selector)
+        {
+            this.source = source;
+            this.selector = selector;
+            threadId = Environment.CurrentManagedThreadId;
+        }
+
+        public TResult Current { get; private set; }
+
+        object IEnumerator.Current => Current;
+
+        public IEnumerator<TResult> GetEnumerator()
+        {
+            if ((state == 0) && (threadId == Environment.CurrentManagedThreadId))
+            {
+                state = 1;
+                return this;
+            }
+
+            return new SelectNotNullArrayIterator<TSource, TResult>(source, selector) { state = 1 };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public bool MoveNext()
+        {
+            var array = source;
+            for (var i = index; (uint)i < (uint)array.Length; i++)
+            {
+                var value = selector(array[i]);
+                if (value.HasValue)
+                {
+                    index = i + 1;
+                    Current = value.GetValueOrDefault();
+                    return true;
+                }
+            }
+
+            index = array.Length;
+            return false;
+        }
+
+        public void Reset() => throw new NotSupportedException();
+
+        public void Dispose()
+        {
+        }
+    }
+
+    private sealed class WhereNotNullListIterator<TResult> : IEnumerable<TResult>, IEnumerator<TResult>
+        where TResult : struct
+    {
+        private readonly List<TResult?> source;
+
+        private readonly int threadId;
+
+        private int state;
+
+        private List<TResult?>.Enumerator enumerator;
+
+        public WhereNotNullListIterator(List<TResult?> source)
+        {
+            this.source = source;
+            threadId = Environment.CurrentManagedThreadId;
+        }
+
+        public TResult Current { get; private set; }
+
+        object IEnumerator.Current => Current;
+
+        public IEnumerator<TResult> GetEnumerator()
+        {
+            if ((state == 0) && (threadId == Environment.CurrentManagedThreadId))
+            {
+                state = 1;
+                enumerator = source.GetEnumerator();
+                return this;
+            }
+
+            return new WhereNotNullListIterator<TResult>(source) { state = 1, enumerator = source.GetEnumerator() };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public bool MoveNext()
+        {
+            while (enumerator.MoveNext())
+            {
+                var value = enumerator.Current;
+                if (value.HasValue)
+                {
+                    Current = value.GetValueOrDefault();
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void Reset() => throw new NotSupportedException();
+
+        public void Dispose() => enumerator.Dispose();
+    }
+
+    private sealed class WhereNotNullArrayIterator<TResult> : IEnumerable<TResult>, IEnumerator<TResult>
+        where TResult : struct
+    {
+        private readonly TResult?[] source;
+
+        private readonly int threadId;
+
+        private int state;
+
+        private int index;
+
+        public WhereNotNullArrayIterator(TResult?[] source)
+        {
+            this.source = source;
+            threadId = Environment.CurrentManagedThreadId;
+        }
+
+        public TResult Current { get; private set; }
+
+        object IEnumerator.Current => Current;
+
+        public IEnumerator<TResult> GetEnumerator()
+        {
+            if ((state == 0) && (threadId == Environment.CurrentManagedThreadId))
+            {
+                state = 1;
+                return this;
+            }
+
+            return new WhereNotNullArrayIterator<TResult>(source) { state = 1 };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public bool MoveNext()
+        {
+            var array = source;
+            for (var i = index; (uint)i < (uint)array.Length; i++)
+            {
+                var value = array[i];
+                if (value.HasValue)
+                {
+                    index = i + 1;
+                    Current = value.GetValueOrDefault();
+                    return true;
+                }
+            }
+
+            index = array.Length;
+            return false;
+        }
+
+        public void Reset() => throw new NotSupportedException();
+
+        public void Dispose()
+        {
         }
     }
 }
